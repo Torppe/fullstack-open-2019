8acaba35a170f4e883f8b48ae73287b63415dbdb
Tuomas Männistö <tuomas.o.mannisto@gmail.com>
2019-07-27 13:09:15 +0300

    puhelinluettelo step6 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filter, setFilter ] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
     const names = persons.map(person => person.name)
 
-    if(names.includes(newName)){
+    if (names.includes(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
       const personObject = {
@@ -51,15 +55,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>Add a new</h2>
       <PersonForm newName={newName}
-                  newNumber={newNumber}
-                  addPerson={addPerson}
-                  handleNameChange={handleNameChange}
-                  handleNumberChange={handleNumberChange}/>
+        newNumber={newNumber}
+        addPerson={addPerson}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons persons={getFilteredPersons()}/>
+      <Persons persons={getFilteredPersons()} />
     </div>
   )
 
