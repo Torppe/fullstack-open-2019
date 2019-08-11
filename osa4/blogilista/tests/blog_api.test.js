cd8de3d0ed845613eb3aedbc79147523a70b11a9
Tuomas Männistö <tuomas.o.mannisto@gmail.com>
2019-08-11 12:52:14 +0300

    4.8 done

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -3,13 +3,23 @@ const supertest = require("supertest")
 const app = require("../app")
 
 const api = supertest(app)
+describe("GET blogs", () => {
+  const path = "/api/blogs"
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
+  test("blogs are returned as json", async () => {
+    await api
+      .get(path)
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+  })
+
+  test("return 2 blogs", async () => {
+    const response = await api.get(path)
+
+    expect(response.body.length).toBe(2)
+  })
 })
+
 
 afterAll(() => {
   mongoose.connection.close()
