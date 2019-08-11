525d846bffa062be66a92c59cc0d69a32faadff0
Tuomas Männistö <tuomas.o.mannisto@gmail.com>
2019-08-11 13:31:06 +0300

    4.9 done

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,8 +1,44 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
-
 const api = supertest(app)
+const Blog = require("../models/blog")
+
+
+const initialBlogs  = [
+  {
+    author:"Testi Herra",
+    title:"Sensational title",
+    url:"www.herrahubertinsivut.com",
+    likes: 5
+  },
+  {
+    author:"Testi Neiti",
+    title:"Sensational title",
+    url:"www.neitihubertinsivut.com",
+    likes: 2
+  },
+  {
+    author:"Testi Keissi",
+    title:"Sensational title",
+    url:"www.neitihubertinsivut.com",
+    likes: 2
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
+
 describe("GET blogs", () => {
   const path = "/api/blogs"
 
@@ -13,10 +49,37 @@ describe("GET blogs", () => {
       .expect("Content-Type", /application\/json/)
   })
 
-  test("return 2 blogs", async () => {
+  test("return 3 blogs", async () => {
     const response = await api.get(path)
 
-    expect(response.body.length).toBe(2)
+    expect(response.body.length).toBe(initialBlogs.length)
+  })
+
+  test("field is called id instead of _id", async () => {
+    const response = await api.get(path)
+    expect(response.body[0].id).toBeDefined()
+  })
+})
+
+describe("POST blogs", () => {
+  const path = "/api/blogs"
+
+  test("blogs are returned as json", async () => {
+    await api
+      .get(path)
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+  })
+
+  test("return 3 blogs", async () => {
+    const response = await api.get(path)
+
+    expect(response.body.length).toBe(initialBlogs.length)
+  })
+
+  test("field is called id instead of _id", async () => {
+    const response = await api.get(path)
+    expect(response.body[0].id).toBeDefined()
   })
 })
 
