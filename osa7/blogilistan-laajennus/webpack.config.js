d4bcf680dbeb796db80f40dcd134756786ec4124
Tuomas Männistö <tuomas.o.mannisto@gmail.com>
2019-09-22 16:30:21 +0300

    7.16 done

---
+++ osa7/blogilistan-laajennus/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path")
+const webpack = require("webpack")
+
+const config = (env, argv) => {
+  console.log("argv", argv.mode)
+
+  const backend_url = "http://localhost:3003"
+
+  return {
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js"
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, "build"),
+      compress: true,
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: backend_url,
+          secure: false,
+          changeOrigin: true
+        }
+      }
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+        {
+          test: /\.css$/,
+          loaders: ["style-loader", "css-loader"],
+        }
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
+  }
+}
+module.exports = config
